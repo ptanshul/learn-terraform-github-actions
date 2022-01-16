4554416925f6991579f503824c0fabb1e33c1296
ptanshul <anshul.tiwari@rediffmail.com>
2022-01-16 22:49:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
 
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"
 }
 
 
@@ -30,9 +30,9 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0af25d0df86db00c1"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
+  
 
   user_data = <<-EOF
               #!/bin/bash
